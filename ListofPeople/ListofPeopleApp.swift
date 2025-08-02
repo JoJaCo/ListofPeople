17a6fc145d87cadb5f66fcae82283f97dc1bc9b3
JoJaCo <contrerasjorge226@gmail.com>
2025-08-01 22:48:04 -0700

    Initial Commit

---
+++ ListofPeople/ListofPeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListofPeopleApp.swift
+//  ListofPeople
+//
+//  Created by Jorge Contreras on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ListofPeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListofPeopleTests/ListofPeopleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ListofPeopleTests.swift
+//  ListofPeopleTests
+//
+//  Created by Jorge Contreras on 8/1/25.
+//
+
+import Testing
+@testable import ListofPeople
+
+struct ListofPeopleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
